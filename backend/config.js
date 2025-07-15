15f0f09f9c9e11966a53b8ebc07b224d40692909
Keegan Leaper <kl308426@maverik.com>
2025-07-15 14:18:38 -0500

    Update config to a named export

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const config = {
+export const config = {
     port: process.env.PORT || 5000,
     dbUrl: process.env.DATABASE_URL,
 };
-
-export default config;
